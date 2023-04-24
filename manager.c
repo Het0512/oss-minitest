7daa063dc329f37ce3615ebd5e2d6262d6ddb19a
het0512 <euntaek.han03@gmail.com>
2023-04-24 21:58:03 +0900

    Update loadData

--- manager.c
+++ manager.c
@@ -51,14 +51,22 @@ void saveData(Product p[], int count){
 
 //파일에서 데이터 불러오는 함수
 int loadData(Product *p){
-	int count=0;
-	FILE*fp;
+	FILE * file = fopen("product.txt", "rt");
 
-	//파일 내용을 읽어와서 배열에 값 추가하기
-
-
-
-
-	printf("=> 로딩 성공!\n");
-	return count;
+	if(file == NULL){
+		printf("=> 파일 없음\n");
+	}
+	else{
+		for(int i; i < 100; i++){
+			fscanf(file, "%d", &p[i].weight);
+			if(feof(file)){
+				break;
+			}
+			fscanf(file, "%d", &p[i].price);
+			fscanf(file, " %[^\n]", p[i].name);
+		}
+		printf("=> 로딩 성공!\n");
+		fclose(file);
+	}
+	return i;
 }
